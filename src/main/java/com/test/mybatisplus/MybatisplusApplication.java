8c6a16c7567bb60da44c7b5e2dd4dc3b9628e203
李洲诚 <eto_li@outlook.com>
2021-09-15 20:45:46 +0800

    项目结构变更

---
+++ src/main/java/com/test/mybatisplus/MybatisplusApplication.java
@@ -0,0 +1,15 @@
+package com.test.mybatisplus;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("mapper")
+public class MybatisplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisplusApplication.class, args);
+    }
+
+}
